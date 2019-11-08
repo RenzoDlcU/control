f43ea3532c28e0510bb928a40350111c496d988c
Renzo <renzo99_0@hotmail.com>
2019-11-07 22:10:56 -0500

    I implemented the methods get and post

--- code/app.py
+++ code/app.py
@@ -4,11 +4,21 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+students = []
+
+
 class Student(Resource):
-    def get(self,name):
-        return {'student': name}
 
+    def get(self, name):
+        for student in students:
+            if student['name'] == name:
+                return student
+        return {'student': None}
 
+    def post(self, name):
+        student = {'name': name, 'lastname': 'Juanes',  'grades': [20,20,20]}
+        students.append(student)
+        return student
 
 api.add_resource(Student, '/student/<string:name>') #http://127.0.0.1:5000/student/<string:name> replace to app.route
 app.run(port=5000)
